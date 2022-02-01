58b954f0102bc50e9aa8b173ad8dc58fe86ed29d
dmiv <Dmitriy40647274@gmail.com>
2022-02-01 14:28:33 +0300

    added check to valid number

--- It_kgb/view.py
+++ It_kgb/view.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from application.models import Users
+import re
 
+
+def valid_number(tel):
+    pattern = re.compile(r'\+?\d{9,15}')
+    return bool(pattern.match(tel))
 
 def index(request):
     if request.POST:
         name = request.POST['Name']
         email = request.POST['Email']
         tel = request.POST['tel']
-        Users.objects.create(name=name, email=email, phone_number=tel)
+        if valid_number(tel):
+            Users.objects.create(name=name, email=email, phone_number=tel)
+        else:
+            message = 'Недопустимый формат номера'
+            return render(request, 'index.html', {'message': message})
     return render(request, 'index.html')
+
+
+
